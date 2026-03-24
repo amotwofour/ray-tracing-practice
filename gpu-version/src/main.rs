f028494db9e763cea03f431c36f8de5e32a30137
amotwofour <aryanmohanty1335@gmail.com>
2026-03-24 14:45:38 -0500

    Updated dependency versions for better functionality. Working on trying to figure out why displaying does not work despite code looking correct

--- gpu-version/src/main.rs
+++ gpu-version/src/main.rs
@@ -1,3 +1,39 @@
-fn main() {
-    println!("Hello, world!");
+use {
+    anyhow::{Context, Result},
+    winit::{
+        event::{Event, WindowEvent},
+        event_loop::{ControlFlow, EventLoop},
+        window::{Window, WindowBuilder},
+    },
+};
+
+const WIDTH: u32 = 800;
+const HEIGHT: u32 = 600;
+
+fn main() -> Result<()> {
+    let event_loop = EventLoop::new()?;
+    let window_size = winit::dpi::PhysicalSize::new(WIDTH, HEIGHT);
+    let window = WindowBuilder::new()
+        .with_inner_size(window_size)
+        .with_resizable(false)
+        .with_title("GPU Path Tracer".to_string())
+        .build(&event_loop)?;
+
+    // TODO: initialize renderer
+
+    event_loop.run(|event, control_handle| {
+        control_handle.set_control_flow(ControlFlow::Poll);
+        match event {
+            Event::WindowEvent { event, .. } => match event {
+                WindowEvent::CloseRequested => control_handle.exit(),
+                WindowEvent::RedrawRequested => {
+                    // TODO: draw frame
+                    window.request_redraw();
+                }
+                _ => (),
+            },
+            _ => (),
+        }
+    })?;
+    Ok(())
 }
